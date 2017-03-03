5cff2da7e3a9e8a9c9ad29426bf91e54ae880315
Jakub Gereg <j.gereg@gmail.com>
2017-03-04 09:42:35 +1300

    Added UsmJsonHelper and Model class for easier maintenance of new usm.json

---
+++ usm/usm/UsmJsonHelper.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace usm
+{
+
+    /// <summary>
+    /// This helper class is used to create new version of default-usm-json
+    /// It does not create it automatically yet, but it create file called ResultFromHelper.txt
+    /// </summary>
+    public static class UsmJsonHelper
+    {
+        private static string UsmJsonVersion = "0.1";
+        public static void CreateNewEmptyUsm()
+        {
+            var author = new ProjectSettings.Author
+            {
+                Name = "Jakub Gereg",
+                NickName = "wramp",
+                WebSiteUrl = "https://github.com/wramp"
+            };
+
+            var projectSettings = new ProjectSettings
+            {
+                GameName = "Skeleton Game",
+                Description = $"This is description created in Unity Skeleton Maker v{UsmJsonVersion}",
+                Url = "https://github.com/wramp",
+                ImageUrl = "https://media.giphy.com/media/MX9r4jOTStUeA/giphy.gif",
+                Authors = new List<ProjectSettings.Author> { author }
+            };
+
+
+            var model = new UsmJsonModel
+            {
+                UsmJsonVersion = UsmJsonVersion,
+                ProjectSettings = projectSettings
+            };
+            /*Add other settings if model changes*/
+
+
+
+            var result = JsonConvert.SerializeObject(model);
+            File.WriteAllText("ResultFromHelper.txt", result);
+        }
+    }
+}
---
+++ usm/usm/UsmJsonModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace usm
+{
+    /// <summary>
+    /// This model shown how usm.json shoulld looks like
+    /// !!!If you do new changes here, you should automatically create new version throught UsmJsonHelper class
+    /// </summary>
+    public class UsmJsonModel
+    {
+        public string UsmJsonVersion { get; set; }
+        public ProjectSettings ProjectSettings { get; set; }
+    }
+
+    public class ProjectSettings
+    {
+        public string GameName { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string ImageUrl { get; set; }
+        public List<Author> Authors { get; set; }
+
+        public class Author
+        {
+            public string Name { get; set; }
+            public string NickName { get; set; }
+            public string WebSiteUrl { get; set; }
+        }
+    }
+
+
+}
