cbfd1e4c2bf12910266e9a96ce7f3cd46da1463c
Jakub Gereg <j.gereg@gmail.com>
2017-03-04 14:58:58 +1300

    main now contains option to create package

--- usm/usm/Properties/AssemblyInfo.cs
+++ usm/usm/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("usm")]
+[assembly: AssemblyTitle("UnitySkeletonMaker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jakub Gereg")]
 [assembly: AssemblyProduct("usm")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
