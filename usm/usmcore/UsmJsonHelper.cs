f62e35e160fb32c18bdcdcf953701774e1dd0202
Jakub Gereg <j.gereg@gmail.com>
2017-03-05 13:33:22 +1300

    Move all files to usmcore project.

--- usm/usmcore/UsmJsonHelper.cs
+++ usm/usmcore/UsmJsonHelper.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
-namespace usm
+namespace usmcore
 {
 
     /// <summary>
--- usm/usmcore/UsmPackageHelper.cs
+++ usm/usmcore/UsmPackageHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 
-namespace usm
+namespace usmcore
 {
     public static class UsmPackageHelper
     {
